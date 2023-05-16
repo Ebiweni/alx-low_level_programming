43d24e73f7be18437f33295bd9cef0b51b3175c3
Ebiweni <maryeveyday@gmail.com>
2023-05-16 06:15:04 -0700

    print using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints to standard output
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
