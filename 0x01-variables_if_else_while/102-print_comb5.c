3093fb91f28fae62f5df0bc12226239499eb9ade
Ebiweni <maryeveyday@gmail.com>
2023-05-20 11:19:52 -0700

    print combo of two two numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,21 +9,25 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 98; ++a)
+	for (a = 0; a <= 98; a++)
 	{
-		for (b = 0 + 1; b <= 99; ++b)
+		for (b = 0; b <= 99; b++)
 		{
-			putchar((a / 10) + '0');
-			putchar((a % 10) + '0');
+			if (a < b && b != a)
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
 
-			putchar(' ');
-
-			putchar((b / 10) + '0');
-			putchar((b % 10) + '0');
-			if (a == 98 && b == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+					break;
+				}
+			}
 
 		}
 	}
