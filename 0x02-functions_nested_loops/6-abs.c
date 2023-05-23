7ad9f2d20ac4be1dc03291200c8e3679449822a2
Ebiweni <maryeveyday@gmail.com>
2023-05-23 05:54:33 -0700

    compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - compute absolute value of a number
+ * @e: character to be checked
+ * Return: 0
+ */
+
+int _abs(int e)
+{
+	if (e >= 0)
+	{
+		return (e);
+	}
+	else
+	{
+		return (e * -1);
+	}
+}
+
