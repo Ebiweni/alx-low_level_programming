125611e3ef9c41d0596cf067871e2b0e163d3b68
Ebiweni <maryeveyday@gmail.com>
2023-05-23 02:48:23 -0700

    print a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print a-z
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int alph;
+
+	for (alph = 'a'; alph <= 'z'; ++alph)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+	return (0);
+}
