4a89417a1bbd0ca4ec5793960204c16bfea7132c
Ebiweni <maryeveyday@gmail.com>
2023-05-23 04:10:26 -0700

    man

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower(int c) - print lowercaseletters
+ *
+ * Return: 1
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && C <= 122)
+		return (1);
+	else
+		return (0);
+	_putchar ('\n');
+}
